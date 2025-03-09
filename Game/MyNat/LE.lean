7b894e6bec3b147e64d56ea75a62429e17eadd2c
Kent Van Vels <kvanvels@protonmail.com>
2025-03-09 14:51:06 -0400

    changes to LE.lean

--- Game/MyNat/LE.lean
+++ Game/MyNat/LE.lean
@@ -10,13 +10,6 @@ namespace MyNat
 
 def le (a b : ℕ) :=  ∃ (c : ℕ), b = a + c
 
--- Another choice is to define it recursively:
--- (kb) note: I didn't choose this option because tests showed
--- that mathematicians found it a lot more confusing than
--- the existence definition.
-
--- | le 0 _
--- | le (succ a) (succ b) = le ab
 
 -- notation
 instance : LE MyNat := ⟨MyNat.le⟩
@@ -45,6 +38,7 @@ instance leDec : DecidableRel ((· ≤ ·) : ℕ  → ℕ → Prop)
     rw [succ_add] at hθ
     have h1 := succ_inj k (j + θ) hθ
     exact h1
+
 
 
 end MyNat
